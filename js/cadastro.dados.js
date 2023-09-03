532c55d30e72819d7bb59939929fd5f15709c87d
Eder Sousa <edersousa@gmail.com>
2023-09-03 15:52:34 -0300

    Atualizando os CSS e JS

---
+++ js/cadastro.dados.js
@@ -0,0 +1,16 @@
+function retornaIndex() {
+	location.href = "index.php"
+}
+
+function mascara_data(campo, valor){
+  var mydata = '';
+  mydata = mydata + valor;
+  if (mydata.length == 2){
+	mydata = mydata + '/';
+	campo.value = mydata;
+  }
+  if (mydata.length == 5){
+	mydata = mydata + '/';
+	campo.value = mydata;
+  }
+}
